e0aa7fadb3a350e28f2309ea21c75b8cc00aa2bc
FinnHofer <finn.hofer@gmail.com>
2025-03-06 14:56:19 +0100

    readability changes

--- tokenizer.py
+++ tokenizer.py
@@ -8,14 +8,14 @@ TRAINING_PATH = './training-data'
 
 def read_training_data(file_path=TRAINING_PATH):
     with open(file_path, "r", encoding="utf-8") as file:
-        return file.read()
+        return file.read().replace("\n", " ")
 
 def read_vocab(file_path=VOCAB_PATH):
     """Reads the existing vocabulary from the given file path."""
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError) as err:
+    except (FileNotFoundError, json.JSONDecodeError):
         return []
 
 def write_vocab(data, file_path=VOCAB_PATH):
@@ -36,6 +36,7 @@ def init_vocab(text):
     return char_vocab
 
 def get_most_common_pair(corpus):
+    """Calculate the most common Pair in a given Corpus"""
     pairs = Counter(zip(corpus, corpus[1:]))
     if not pairs:
         return None
@@ -59,10 +60,10 @@ def merge_corpus(corpus, merged_pair):
     return merged_corpus
 
 def train_vocab(iterations):
+    """Train the Vocabulary based on the Training-Files"""
     for file in os.listdir(TRAINING_PATH):
         if file.endswith('.txt'):
-            print("PROCESSING: " + file)
-            text = read_training_data(TRAINING_PATH + '/' + file).replace("\n", " ")
+            text = read_training_data(TRAINING_PATH + '/' + file)
 
             corpus = list(text)
             existing_vocab = defaultdict(int, read_vocab())
@@ -70,7 +71,7 @@ def train_vocab(iterations):
             
             for _ in range(iterations):
                 most_common_pair = get_most_common_pair(corpus)
-                
+
                 corpus = merge_corpus(corpus, most_common_pair[0])
                 vocab[most_common_pair[0]] = most_common_pair[1]
 
