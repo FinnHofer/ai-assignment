c667b80efdf529005fb5eeaecb436ac7d5625888
FinnHofer <finn.hofer@gmail.com>
2025-04-25 20:28:12 +0200

    create vocab for earch language file

--- bpe-tokenizer/bpe-tokenizer.py
+++ bpe-tokenizer/bpe-tokenizer.py
@@ -2,27 +2,27 @@ from collections import Counter, defaultdict
 import json
 import os
 
-VOCAB_PATH = './bpe-tokenizer/output/vocab.json'
-TRAINING_PATH = './bpe-tokenizer/training-data'
+VOCAB_PATH = './output'
+TRAINING_PATH = './training-data'
 
 def read_training_data(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        return file.read().replace("\n", " ")
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read().replace('\n', ' ')
 
 def read_vocab(file_path=VOCAB_PATH):
     try:
-        with open(file_path, "r", encoding="utf-8") as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             return json.load(file)
     except (FileNotFoundError, json.JSONDecodeError) as error:
         print(error)
         return []
 
-def write_vocab(data, file_path=VOCAB_PATH):
+def write_vocab(data, file_name='vocab.json', folder_path=VOCAB_PATH):
     try:
-        with open(file_path, "w", encoding="utf-8") as file:
+        with open(folder_path+'/'+file_name, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4)
     except (TypeError, ValueError) as err:
-        raise RuntimeError(f"Failed to write vocabulary: {err}")
+        raise RuntimeError(f'Failed to write vocabulary: {err}')
 
 def init_vocab(text):
     char_vocab = defaultdict(int)
@@ -39,7 +39,7 @@ def get_most_common_pair(corpus):
     
     print(pairs.most_common(1))
     most_common_pair = pairs.most_common(1)[0]
-    return ("".join(most_common_pair[0]), most_common_pair[1])
+    return (''.join(most_common_pair[0]), most_common_pair[1])
 
 def merge_corpus(corpus, merged_pair):
     merged_corpus = []
@@ -47,7 +47,7 @@ def merge_corpus(corpus, merged_pair):
     while i < len(corpus):
         if i < len(corpus) - 1 and (corpus[i] + corpus[i + 1]) == merged_pair:
             merged_corpus.append(merged_pair)
-            i += 2  # Skip the next token as it's merged
+            i += 2
         else:
             merged_corpus.append(corpus[i])
             i += 1
@@ -61,8 +61,7 @@ def train_vocab(iterations):
             print('PROCESSING FILE: ' + file)
 
             corpus = list(text)
-            existing_vocab = defaultdict(int, read_vocab())
-            vocab = existing_vocab if existing_vocab else init_vocab(text)
+            vocab = init_vocab(text)
             
             for _ in range(iterations):
                 most_common_pair = get_most_common_pair(corpus)
@@ -72,10 +71,11 @@ def train_vocab(iterations):
                 corpus = merge_corpus(corpus, most_common_pair[0])
                 vocab[most_common_pair[0]] = most_common_pair[1]
 
-            write_vocab(dict(sorted(vocab.items(), key=lambda item: item[1])))
+            vocab_file_name = file.replace('.txt', '-vocab.json')
+            write_vocab(dict(sorted(vocab.items(), key=lambda item: item[1])), vocab_file_name)
 
-def tokenize(word):
-    vocab = defaultdict(int, read_vocab())
+def tokenize(word, vocab_path):
+    vocab = defaultdict(int, read_vocab(vocab_path))
     subwords = []
 
     while word:
@@ -86,11 +86,11 @@ def tokenize(word):
                 word = word[i:]
                 break
         else:
-            subwords.append("<UNKNOWN>")
+            subwords.append('<UNKNOWN>')
             break
         
     return subwords
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     #train_vocab(300)
-    print(tokenize('hello world'))
+    print(tokenize('hello world', VOCAB_PATH + '/' + 'training-de-vocab.json'))
